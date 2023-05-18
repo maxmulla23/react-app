03773280a2ed9f2c5009bb919c51fc7528711afd
maxmulla23 <maxmulla23@gmail.com>
2023-05-18 16:28:46 +0300

    react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,10 @@ const Header = ({title}) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick} />
+        {location.pathname === '/' && (
+          <Button color='green' text='Add' onClick={onClick} />
+        )}
+        
     </header>
   )
 }
