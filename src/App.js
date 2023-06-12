a559fcf711bba8b0ff3e796780863ba1a74e6da1
maxmulla23 <maxmulla23@gmail.com>
2023-06-12 14:38:34 +0300

    json

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true, 
-    },
-    {
-        id: 3,
-        text: 'Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-    
-])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  //fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:6000/tasks')
+
+    const data = await res.json()
+
+    console.log(data);
+    return data
+  }
 
 //Add task
  const addTask = (task) =>{
@@ -52,9 +51,12 @@ const toggleReminder = (id) =>{
     
     <div className="container ">
       <Header 
-        onAdd={() => setShowAddTask(!showAddTask)} 
-        showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
+        onAdd={() => setShowAddTask
+          (!showAddTask)} 
+          showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} 
+      />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete= 
         {deleteTask} onToggle={toggleReminder} /> 
